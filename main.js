d1f701b004800070fb1a8c141972dcc411b3faf5
mintalan <intalan.mf@obmontessori.edu.ph>
2025-05-06 08:30:58 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function calculateAverage() {
+    var filipino = document.getElementById('filipino').value;
+    var english = document.getElementById('english').value;
+    var science = document.getElementById('science').value;
+
+    var average = (filipino * 1 + english * 1 + science * 1) / 3;
+
+    var rating = "";
+
+    if (average >= 94) {
+        rating = "Excellent";
+    } else if (average >= 87) {
+        rating = "Above Satisfactory";
+    } else if (average >= 80) {
+        rating = "Satisfactory";
+    } else if (average >= 75) {
+        rating = "Needs Improvement";
+    } else {
+        rating = "Poor";
+    }
+
+    alert("Average: " + average.toFixed(2) + "\nRating: " + rating);
+}
